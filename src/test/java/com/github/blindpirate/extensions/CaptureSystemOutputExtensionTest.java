3f29f547653af4e428849a864e781635e7bfe707
JiangTaoHou <925147954@qq.com>
2023-07-13 21:34:51 +0800

    再次添加注释

--- src/test/java/com/github/blindpirate/extensions/CaptureSystemOutputExtensionTest.java
+++ src/test/java/com/github/blindpirate/extensions/CaptureSystemOutputExtensionTest.java
@@ -5,6 +5,22 @@ import org.junit.jupiter.api.Test;
 import static org.hamcrest.Matchers.containsString;
 
 public class CaptureSystemOutputExtensionTest {
+    /**
+     * 为什么@CaptureSystemOutput必须和@Test一起才能发挥作用呢？因为@CaptureSystemOutput
+     * 注解上面贴着一个@ExtendWith(CaptureSystemOutputExtension.class)
+     * 说明要去拓展junit5的东西了，然后
+     *
+     class CaptureSystemOutputExtension implements
+     BeforeEachCallback, AfterEachCallback, ParameterResolver
+     因为这个类实现了BeforeEachCallback, AfterEachCallback,这两个接口，
+     就相当于有了在所有的方法前，方法后进行操作
+     而实现了ParameterResolver之后就自定义了是否生效，
+     boolean isOutputCapture = parameterContext.getParameter().getType() == OutputCapture.class;
+     这句代码就规定了参数必须是CaptureSystemOutput.OutputCapture类型的，
+     然后因为把Java自带的输出流给替换了，所以可以捕捉到。最终使用hamcrest进行去比较字符串
+     *
+     *
+     **/
     @Test
     @CaptureSystemOutput
     void systemOut(CaptureSystemOutput.OutputCapture outputCapture) {
